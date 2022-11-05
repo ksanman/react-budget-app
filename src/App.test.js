b00bfc76a579c7d0c439bde3f6136ea55aad3d5a
ksanman <agent.xaber@gmail.com>
2022-11-05 13:19:14 -0600

    fix unit test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,14 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
-
+import { Provider } from 'react-redux';
+import store from './store';
 
 describe('App', () => {
     test('renders App component', () => {
-      render(<App />);
+      render(
+        <Provider store={store}>
+            <App />
+        </Provider>
+      );
     });
   });
